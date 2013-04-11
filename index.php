90e8ee0c27c04c246f1e6d3643ad240edf22bb32
Eduardo García <eg@manoderecha.mx>
2013-04-10 22:42:28 -0500

    Adds Google Analytics tracking code.

--- index.php
+++ index.php
@@ -22,6 +22,18 @@
 	<link href="static/css/flat-ui.css" rel="stylesheet">
 	<!--<link href="static/css/cosmo.bootstrap.min.css" rel="stylesheet">-->
 	<link href="static/css/app.css" rel="stylesheet">
+
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-40008557-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+</script>
 </head>
 
 <body ng-app="cvitae" ng-controller="CvitaeCtrl">
