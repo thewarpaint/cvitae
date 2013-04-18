f32250c82063e87c73d129f26f71b84ced680987
Eduardo García <eg@manoderecha.mx>
2013-04-18 10:25:45 -0500

    Adds viewport meta tag to fix mobile issues.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.00" />
 	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js"></script>
 	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
